a90ab19efad62167784177a847e04dbb2c8dca12
laZuR1s <sashalazyr@gmail.com>
2024-02-20 15:04:40 +0300

    a little changes

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -10,8 +10,13 @@ template <typename T, typename Predicat>
 void Read_and_check(T& x, std::istream& stream, Predicat condition, const char* message);
 int getRandomInInterval(int a, int b);
 void print(int* arr, int size, const char* message);
-void preambStatic(short choice, std::ifstream& file, int& size);
-void preambDynamic(short choice, std::ifstream& file, int& size);
+void preambDynamic(short choice, std::ifstream& file, int& Rows, int& Colums);
+void fill_row(int* arr, int a, int b, int Colums = Colums1);
+void fill_row(int* arr, std::istream& stream, int Colums = Colums1);
+void fill_matrix(int matrix[Rows1][Colums1], int a, int b);
+void fill_matrix(int matrix[Rows1][Colums1], std::istream& stream = std::cin);
+void fill_matrix(int** matrix, int Rows, int Colums, std::istream& stream = std::cin);
+void fill_matrix(int** matrix, int a, int b, int Rows, int Colums);
 
 // Функции связанные с памятью, выделяемой для массивов
 int** memory_allocation(int newRows, int newColums);
@@ -43,7 +48,7 @@ int main()
 				if (choice != 4)
 				{
 					std::cout << "\nВыбран пункт меню: '" << choice << "'\n";
-					
+
 
 					switch (mainChoice)
 					{
@@ -51,12 +56,13 @@ int main()
 					{
 						int size;
 						std::ifstream file("data.txt");
-						preambStatic(choice, file, size);
 						int matrix[Rows1][Colums1];
+
 						switch (choice)
 						{
 						case 1:
 						{
+							std::cout << "Количество столбцов = " << Colums1 << ' ' << "Количество строк = " << Rows1 << '\n';
 
 							break;
 						}
@@ -67,6 +73,7 @@ int main()
 						}
 						default:
 						{
+							std::cout << "Количество столбцов = " << Colums1 << ' ' << "Количество строк = " << Rows1 << '\n';
 
 						}
 						}
@@ -76,7 +83,9 @@ int main()
 					{
 						int size;
 						std::ifstream file("data.txt");
-						preambDynamic(choice, file, size);
+						int Rows = 0, Colums = 0;
+
+						preambDynamic(choice, file, Rows, Colums);
 
 						switch (choice)
 						{
@@ -217,56 +226,60 @@ void print(int* arr, int size, const char* message)
 	std::cout << '\n';
 }
 
-void preambStatic(short choice, std::ifstream& file, int& size)
+
+void preambDynamic(short choice, std::ifstream& file, int& Rows, int& Colums)
 {
 	switch (choice)
 	{
 	case 1:
 	{
-		std::cout << " \nМатрица из "<<Rows1<<" строк и "<<Colums1<<" столбцов ";
+		Read_and_check(Rows, std::cin, [](int x) {return x > 0; }, "Введите количество строк квадратной матрицы\n->");
+		Read_and_check(Colums, std::cin, [Rows](int x) {return x > 0 && Rows == x; }, "Введите количество столбцов квадратной матрицы\n->");
+		std::cin.ignore(std::cin.rdbuf()->in_avail());
 		std::cin.ignore(std::cin.rdbuf()->in_avail());
 		break;
 	}
 	case 2:
 	{
 		if (isFileWithContent(file))
-			file >> size;
+			file >> Rows >> Colums;
 		break;
 	}
 	default:
 	{
 		std::cout << "\nВведите количество случайных слагаемых: ";
-		Read_and_check(size, std::cin, [](int x) {return x > 0; }, "\n-> ");
+		Read_and_check(Rows, std::cin, [](int x) {return x > 0; }, "Введите количество случайных строк\n->");
+		Read_and_check(Colums, std::cin, [Rows](int x) {return x > 0 && Rows == x; }, "Введите количество случайных столбцов\n->");
 		std::cin.ignore(std::cin.rdbuf()->in_avail());
 	}
 	}
 }
-void preambDynamic(short choice, std::ifstream& file, int& Rows,int& Colums)
+void fill_row(int* arr, std::istream& stream, int Colums)
 {
-	switch (choice)
-	{
-	case 1:
-	{
-		Read_and_check(Rows,std::cin, [](int x) {return x > 0; }, "Введите количество строк квадратной матрицы\n->");
-		Read_and_check(Colums,std::cin, [Rows](int x) {return x > 0 && Rows == x; }, "Введите количество столбцов квадратной матрицы\n->");
-		std::cin.ignore(std::cin.rdbuf()->in_avail());
-		std::cin.ignore(std::cin.rdbuf()->in_avail());
-		break;
-	}
-	case 2:
-	{
-		if (isFileWithContent(file))
-			file >> Rows>>Colums;
-		break;
-	}
-	default:
-	{
-		std::cout << "\nВведите количество случайных слагаемых: ";
-		Read_and_check(Rows,std::cin, [](int x) {return x > 0; }, "Введите количество случайных строк\n->");
-		Read_and_check(Colums,std::cin, [Rows](int x) {return x > 0 && Rows == x; }, "Введите количество случайных столбцов\n->");
-		std::cin.ignore(std::cin.rdbuf()->in_avail());
-	}
-	}
+	for (int i{ 0 }; i < Colums; ++i)
+		stream >> arr[i];
+}
+
+void fill_matrix(int matrix[Rows1][Colums1], int a, int b)
+{
+	for (int i{ 0 }; i < Rows1; ++i)
+		fill_row(matrix[i], a, b);
+}
+
+void fill_matrix(int matrix[Rows1][Colums1], std::istream& stream)
+{
+	for (int i{ 0 }; i < Rows1; ++i)
+		fill_row(matrix[i], stream);
+}
+
+void fill_matrix(int** matrix, int Rows, int Colums, std::istream& stream) {
+	for (int i{ 0 }; i < Rows; ++i)
+		fill_row(matrix[i], stream, Colums);
+}
+
+void fill_matrix(int** matrix, int a, int b, int Rows, int Colums) {
+	for (int i{ 0 }; i < Rows; ++i)
+		fill_row(matrix[i], a, b, Colums);
 }
 
 
